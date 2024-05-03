d815294e20c452a0a173d9cf8cde9ac836b69374
bright-kumedzro <kumedzrobright@gmail.com>
2024-05-03 03:15:49 +0000

    Day 3

--- Day 03/pizzaOrder.py
+++ Day 03/pizzaOrder.py
@@ -1,4 +1,4 @@
-ech# Congratulations! You've got a job at Python Pizza. Your first job is to build an automatic pizza order program.
+# Congratulations! You've got a job at Python Pizza. Your first job is to build an automatic pizza order program.
 # Based on a user's order, work out their final bill.
 # Small Pizza : $15
 # Medium Pizza : $20
@@ -11,9 +11,50 @@ ech# Congratulations! You've got a job at Python Pizza. Your first job is to bui
 
 print("Welcome to the Python Pizza Deliveries!")
 
-size = input("What Pizza size do you want? S, M, or L?")
+size = input("What Pizza size do you want? S, M, or L?\n")
 
-add_pepperoni = input("Do you want Pepperoni? Yes or No?")
+add_pepperoni = input("Do you want Pepperoni? Yes or No?\n")
 
-extra_cheese = input("Do you want extra cheese? Yes or NO?")
+extra_cheese = input("Do you want extra cheese? Yes or No?\n")
 
+if size == "S":
+    bill = 15
+    if add_pepperoni == "Yes":
+        bill += 2
+        if extra_cheese == "Yes":
+            bill += 1
+        else:
+            pass
+    else:  # add_pepperoni == "No"
+        if extra_cheese == "Yes":
+            bill += 1
+        else:
+            pass
+elif size == "M":
+    bill = 20
+    if add_pepperoni == "Yes":
+        bill += 3
+        if extra_cheese == "Yes":
+            bill += 1
+        else:
+            pass
+    else:  # add_pepperoni == "No"
+        if extra_cheese == "Yes":
+            bill += 1
+        else:
+            pass
+elif size == "L":
+    bill = 25
+    if add_pepperoni == "Yes":
+        bill += 3
+        if extra_cheese == "Yes":
+            bill += 1
+        else:
+            pass
+    else: # add_pepperoni == "No"
+        if extra_cheese == "Yes":
+            bill += 1
+        else:
+            pass
+
+print(f"Your final bill is: ${bill}.")

16e52ac6e596812595f158a169bc12bee2ffbc2f
bright-kumedzro <kumedzrobright@gmail.com>
2024-05-06 11:22:29 +0000

    Final changes to code

--- Day 04/rockPaperScissors.py
+++ Day 04/rockPaperScissors.py
@@ -29,40 +29,46 @@ scissors = '''
 ---.__(___)
 '''
 
+game_images = [rock, paper, scissors]
+
+# accept user input
+
 input_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+# print user choice
+if input_choice >= 0 and input_choice<= 2 :
+    print(game_images[input_choice])
+
+# generate computer choice
 
 computer_choice = random.randint(0, 2)
 
-if input_choice == 0:
-    print(rock)
+# print computer choice
+
+print(f"Computer chose: {game_images[computer_choice]}")
+
+if input_choice == 0:             # Rock
     if computer_choice == 0:
-        print(rock)
         print("It's a draw.")
     elif computer_choice == 1:
-        print(paper)
         print("You lose.")
     else:
-        print(scissors)
         print("You win.")
-elif input_choice == 1:
-    print(paper)
+elif input_choice == 1:           # Paper
+    print(game_images[input_choice])
     if computer_choice == 0:
-        print(rock)
         print("You win.")
     elif computer_choice == 1:
-        print(paper)
         print("It's a draw.")
     else:
-        print(scissors)
         print("You lose.")
-else:
-    print(scissors)
+elif input_choice == 2:          # Scissors
+    print(game_images[input_choice])
     if computer_choice == 0:
-        print(rock)
         print("You lose.")
     elif computer_choice == 1:
-        print(paper)
         print("You win.")
     else:
-        print(scissors)
         print("It's a draw.")
+else:                             # Invalid choice
+    print("Invalid choice. You lose!")
